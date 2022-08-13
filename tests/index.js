2565334c1f1b8586ab0fed5ffe7edf4f7e9e0d9d
felipeog <17603069+felipeog@users.noreply.github.com>
2022-08-13 13:58:29 -0300

    Add tests script

---
+++ tests/index.js
@@ -0,0 +1,34 @@
+const { execSync } = require("child_process");
+const path = require("path");
+const glob = require("glob");
+
+function tests() {
+  const testsGlobPattern = path.resolve(__dirname, "../projects/**/*.tst");
+  const testsPaths = glob.sync(testsGlobPattern);
+  let failCount = 0;
+
+  console.log(`${testsPaths.length} tests`);
+
+  for (let index = 0; index < testsPaths.length; index++) {
+    const testPath = testsPaths[index];
+    const result = execSync(`npm run hdl-js -- --script ${testPath}`, {
+      encoding: "utf-8",
+    });
+
+    console.log(`\n${index + 1}/${testsPaths.length}: ${testPath}`);
+
+    if (!result.includes("✓ Script executed successfully!")) {
+      failCount += 1;
+
+      console.log("> Fail");
+
+      continue;
+    }
+
+    console.log("> Success");
+  }
+
+  console.log(`Success: ${testsPaths.length - failCount}; Fail: ${failCount}`);
+}
+
+tests();
