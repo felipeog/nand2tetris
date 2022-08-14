59a770695886be1ca2e3c5ffc76fe8960d2889f7
felipeog <17603069+felipeog@users.noreply.github.com>
2022-08-13 21:05:43 -0300

    Move package; Improve output

--- tests/index.js
+++ tests/index.js
@@ -1,36 +1,50 @@
 const { execSync } = require("child_process");
 const path = require("path");
 const glob = require("glob");
+const cliProgress = require("cli-progress");
+
+function logResults(results) {
+  for (let index = 0; index < results.length; index++) {
+    const testPath = results[index];
+    const testName = testPath.match(/\w+\.tst/gi)[0];
+
+    console.log(testName);
+  }
+}
 
 function tests() {
   const testsGlobPattern = path.resolve(__dirname, "../projects/**/*.tst");
   const testsPaths = glob.sync(testsGlobPattern);
-  let failCount = 0;
+  const progressBar = new cliProgress.SingleBar();
+  const results = {
+    success: [],
+    fail: [],
+  };
 
-  console.log(`${testsPaths.length} tests`);
+  progressBar.start(testsPaths.length, 0);
 
   for (let index = 0; index < testsPaths.length; index++) {
+    progressBar.update(index + 1);
+
     const testPath = testsPaths[index];
-    const result = execSync(`npm run hdl-js -- --script ${testPath}`, {
+    const testResult = execSync(`npm run hdl-js -- --script ${testPath}`, {
       encoding: "utf-8",
     });
 
-    console.log(`\n${index + 1}/${testsPaths.length}: ${testPath}`);
-
-    if (!result.includes("✓ Script executed successfully!")) {
-      failCount += 1;
-
-      console.log("> Fail");
-
-      continue;
+    if (testResult.includes("✓ Script executed successfully!")) {
+      results.success.push(testPath);
+    } else {
+      results.fail.push(testPath);
     }
-
-    console.log("> Success");
   }
 
-  console.log(
-    `\nSuccess: ${testsPaths.length - failCount}; Fail: ${failCount}`
-  );
+  progressBar.stop();
+
+  console.log("\n- Success: ");
+  logResults(results.success);
+
+  console.log("\n- Fail: ");
+  logResults(results.fail);
 }
 
 tests();
